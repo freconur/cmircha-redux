68074eb32ba38adb8fe048ce59bbf8762a071c6b
freco <freconur@gmail.com>
2022-06-18 22:24:04 -0500

    contador mejorado

--- src/contador/index.js
+++ src/contador/index.js
@@ -1,35 +1,19 @@
 import React from "react";
 import { useReducer } from "react";
+import { contadorInit, contadorInitialState, contadorReducer } from "./reducers/contadorReducer";
+import { TYPES } from "./actions/contadorActions";
 const Contador = () => {
 
 	//reduer es la funcion q va tener la logica para nuestro estado
 	const initialState = {contador: 0}
-	
-	const TYPES = {
-		INCREMENT : "INCREMENT",
-		INCREMENT_5 : "INCREMENT_5",
-		DECREMENT : "DECREMENT",
-		DECREMENT_5 : "DECREMENT_5",
-		RESET: "RESET"
-	}
-	function reducer (state, action) {
- 
-		switch(action.type){
-			case TYPES.INCREMENT:
-				return {contador: state.contador + 1};
-			case TYPES.INCREMENT_5:
-				return {contador: state.contador + action.payload};
-			case TYPES.DECREMENT:
-				return {contador: state.contador - 1};
-			case TYPES.DECREMENT_5:
-				return {contador: state.contador - action.payload};
-			case TYPES.RESET:
-				return initialState
-			default:
-			return state
+	const init = (initialState) => {
+		return {
+			contador: initialState.contador + 100
 		}
 	}
-	const [state, dispatch] = useReducer(reducer, initialState)
+	
+	
+	const [state, dispatch] = useReducer(contadorReducer, contadorInitialState, contadorInit)
   // const [count, setCount] = React.useState(0);
 	const sumar = () => dispatch({type: TYPES.INCREMENT}) 
 	const sumar5 = () => dispatch({type: TYPES.INCREMENT_5, payload:5}) 
